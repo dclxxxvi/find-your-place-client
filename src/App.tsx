b3b48db7f028c961450eada1908c1dbcda5143d4
Александр <t.aleksandr888@mail.ru>
2023-02-24 20:21:19 +0500

    Описание структуры и добавление Public API. Добавление .env файлов и режима локальной разработки. Конфигурация стора, роутера

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import AppRouter from './router';
+import { Provider } from 'react-redux';
+import { store } from './redux';
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-
-		</div>
+		<Provider store={store}>
+			<AppRouter />
+		</Provider>
 	);
 };
 
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+const AppRouter: React.FC = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default AppRouter;
