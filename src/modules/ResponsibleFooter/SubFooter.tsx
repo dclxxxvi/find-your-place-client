ab181cc729b533ff077fd90b4a1c27d0db7f9cb4
Simernope <simeon02908@gmail.com>
2023-05-22 13:57:51 +0500

    fixing subfooter component

--- src/modules/ResponsibleFooter/SubFooter.tsx
+++ src/modules/ResponsibleFooter/SubFooter.tsx
@@ -3,25 +3,33 @@ import { PageWrapper } from '../../components';
 import { Col, Row, Typography } from 'antd';
 import AppTitle from '../../modules/ResponsibleNavbar/components/AppTitle';
 import { Content } from 'antd/es/layout/layout';
+import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint';
 
 const SubFooter: React.FC = () => {
+	const { xs } = useBreakpoint(true);
 	return (
 		<Content>
 			<PageWrapper marginTop={16} marginBottom={16}>
-				<Row justify={'space-between'} align={'middle'}>
-					<Col>
-						<AppTitle />
+				<Row justify={'space-between'} align={xs ? 'top' : 'middle'}>
+					<Col xs={9} sm={8}>
+						<Row>
+							<AppTitle />
+						</Row>
 					</Col>
-					<Col>
-						<Typography.Text>
+					<Col xs={15} sm={8}>
+						<Row justify={xs ? 'end' : 'center'} >
+							<Typography.Text>
 							2023 Все права защищены
-						</Typography.Text>
+							</Typography.Text>
 
+						</Row>
 					</Col>
-					<Col>
-						<Typography.Text>
+					<Col xs={12} sm={8} offset={xs ? 12 : 0}>
+						<Row justify={'end'} >
+							<Typography.Text>
 						+ 7 900 800 32 12
-						</Typography.Text>
+							</Typography.Text>
+						</Row>
 					</Col>
 				</Row>
 			</PageWrapper>
