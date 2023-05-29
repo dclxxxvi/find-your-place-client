d8f9dd632a63ea5ea379ddcbeab0b18a2503caef
Simernope <simeon02908@gmail.com>
2023-05-29 10:23:17 +0500

    fixing title

--- src/modules/ResponsibleNavbar/components/AppTitle.tsx
+++ src/modules/ResponsibleNavbar/components/AppTitle.tsx
@@ -4,10 +4,16 @@ import { useNavigate } from 'react-router-dom';
 
 const AppTitle: React.FC = () => {
 	const navigate = useNavigate();
-
 	return (
 		<Typography.Title
-			style={{ cursor: 'pointer', margin: 0 }}
+			style={{
+				cursor: 'pointer',
+				margin: 0,
+				background: 'linear-gradient(278.07deg, #002766 0%, #096DD9 98.45%)',
+				WebkitBackgroundClip: 'text',
+				WebkitTextFillColor: 'transparent',
+				backgroundClip: 'text',
+			}}
 			onClick={() => navigate('/')}
 			level={2}
 		>
