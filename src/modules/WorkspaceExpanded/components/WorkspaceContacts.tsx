a32b115f60dbe13ea6d44d5e92c3572064692602
Александр <t.aleksandr888@mail.ru>
2023-06-04 21:34:13 +0500

    Added contacts to WorkspaceOverview

--- src/modules/WorkspaceExpanded/components/WorkspaceContacts.tsx
+++ src/modules/WorkspaceExpanded/components/WorkspaceContacts.tsx
@@ -13,14 +13,14 @@ const WorkspaceContacts: React.FC<Props> = ({ phoneNumber, siteUrl }) => {
 	return (
 		<Space id={EAnchorTabs.CONTACTS} direction={'vertical'} size={'large'} style={{ width: '100%' }}>
 			<Title style={{ margin: 0 }} level={3}>{EAnchorTabsLabels.CONTACTS}</Title>
-			<Row>
-				<Col span={12} xs={24} md={12}>
+			<Row gutter={[24, 24]}>
+				<Col span={12} xs={24} sm={12}>
 					<Space direction={'vertical'}>
 						<Typography.Title level={5} style={{ margin: 0 }}>Номер телефона</Typography.Title>
 						<Typography.Text>{phoneNumber || 'Не указан'}</Typography.Text>
 					</Space>
 				</Col>
-				<Col span={12} xs={24} md={12}>
+				<Col span={12} xs={24} sm={12}>
 					<Space direction={'vertical'}>
 						<Typography.Title level={5} style={{ margin: 0 }}>Сайт</Typography.Title>
 						<Typography.Text>{siteUrl || 'Не указан'}</Typography.Text>
